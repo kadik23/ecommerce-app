2e74d7af946a09bffc71b87120017fad7f16072f
kadik23 <kadiksalah03@gmail.com>
2023-10-03 23:14:59 +0100

    commit..

--- resources/views/components/Select.blade.php
+++ resources/views/components/Select.blade.php
@@ -1,4 +1,4 @@
-<form id="filterForm" action="" method="GET">
+<form id="filterForm" action="{{ route('product.filter') }}" method="GET">
     <select id="filter" name="filter" class="  text-left border text-lg border-gray-200 text-gray-900 shadow rounded-lg hover:border-regal-brown transition-all duration-300  focus:ring-regal-brown focus:border-regal-brown block w-72   px-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-regal-brown dark:focus:border-regal-brown">
         <option selected>{{$option}}</option>
         <option value="Available"> Available</option>
