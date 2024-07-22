333aaa563dd393ce58083fe1048ab0c1b00038bc
kadik23 <kadiksalah03@gmail.com>
2024-07-22 19:39:51 +0100

    Fixed some errors

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-cd frontend
-
-echo "# Installing dependencies..."
-npm install
-
-echo "# Building..."
-npm run build
-
-cd ../
-
 echo "# JWT Secret Generate..."
 php artisan jwt:secret
 
@@ -18,9 +8,3 @@ php artisan storage:link
 
 echo "# Start the PHP application in the background"
 php artisan serve --host=0.0.0.0 --port=8000 &
-
-
-cd frontend
-
-echo "# Start the Node.js application concurrently"
-npm run preview
