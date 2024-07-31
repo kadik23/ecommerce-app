8d806a68b9efc4672845eee8b5660916d79dcfe0
kadik23 <kadiksalah03@gmail.com>
2024-07-31 22:35:45 +0100

    Fixed some issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-echo "# Generating JWT Secret..."
+# Navigate to the frontend directory
+cd frontend
+
+echo "# Installing dependencies..."
+npm install
+
+echo "# Building..."
+npm run build
+
+cd ../
+
+echo "# JWT Secret Generate..."
 php artisan jwt:secret
 
-echo "# Linking storage..."
+echo "# Link storage..."
 php artisan storage:link
 
-echo "# Starting the Laravel application"
-php artisan serve --host=0.0.0.0 --port=8000
+echo "# Start the PHP application in the background"
+php artisan serve --host=0.0.0.0 --port=8000 &
+
+# Navigate back to the frontend directory
+cd frontend
+
+echo "# Start the Node.js application concurrently"
+npm run preview
