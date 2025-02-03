1fa9e5e59cce6fb85db3d9222bec452c912db5d7
kadik23 <kadiksalah03@gmail.com>
2025-02-03 16:14:56 +0100

    fix deployment issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,7 @@ php artisan jwt:secret
 echo "# Linking storage..."
 php artisan storage:link
 
+npm run build
+npm run dev
 echo "# Starting the PHP application..."
 php artisan serve --host=0.0.0.0 --port=8000
