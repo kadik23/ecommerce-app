1559ce9bb6f9ad5cc2d0134b3de9de2e2419d7d0
kadik23 <kadiksalah03@gmail.com>
2025-02-03 16:58:36 +0100

    fix ci/cd issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,6 @@ php artisan storage:link
 echo "# Building frontend assets..."
 npm install
 npm run build
-npm run dev
+npm run dev &
 echo "# Starting the PHP application..."
 php artisan serve --host=0.0.0.0 --port=8000
